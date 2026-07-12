1c447ab7b7edd4334312900dde2553b26e5a0134
김준혁 <jh0921abc@gmail.com>
2026-07-12 09:37:52 +0900

    Authorisation Bypass 소스코드 업로드

---
+++ DVWA 보고서/Source/Authorisation Bypass_Source.php
@@ -0,0 +1,10 @@
+<?php
+
+// Low 레벨은 권한 검증 로직이 존재하지 않아 누구든 페이지 접근 가능
+
+/*
+Nothing to see here for this vulnerability, have a look
+instead at the dvwaHtmlEcho function in:
+* dvwa/includes/dvwaPage.inc.php
+*/
+?>
